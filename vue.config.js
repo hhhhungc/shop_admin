90f09dcc6d4667ca76851ad869025291f7ba3dd1
hhhhungc <dial0122@gmail.com>
2022-04-12 17:23:41 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/shop/' : '/',
   chainWebpack: (config) => {
     // 发布模式
     config.when(process.env.NODE_ENV === 'production', (config) => {
